96e839bd5a062e93f0c852bcb568c98e6cee0d0b
Chaz McGinty <bookings@gmail.com>
2021-12-16 23:49:53 +0100

    / ‘typewriter.py’

--- typewriter.py
+++ typewriter.py
@@ -1,6 +1,6 @@
 import sys, time
 
-text = "I wanna be adopted by michael jordan"
+text = "It's kind of hard bein' Snoop D-O-double-G... But I, somehow, some way... Keep comin' up with funky ass shit like every single day... May I, kick a little something for the Gs (yeah)... And, make a few ends as I breeze, through... Two in the mornin' and the partys still jumpin'... 'Cause my momma ain't home"
 
 def typewriter(message):
     for char in message:
