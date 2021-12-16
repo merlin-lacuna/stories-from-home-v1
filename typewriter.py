0f0c2f38f4181c333b15ddb588c42f4dd47c17b6
Chaz McGinty <bookings@gmail.com>
2021-12-16 23:42:54 +0100

    / ‘.idea/workspace.xml’ / ‘typewriter.py’

--- typewriter.py
+++ typewriter.py
@@ -18,7 +18,7 @@ def type_input(message):
     for char in message:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.05)
     return input()
 
 
