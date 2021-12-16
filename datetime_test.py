c7429d12240dafdc19498b45b5871b7b514f2d13
Chaz McGinty <bookings@gmail.com>
2021-12-16 23:28:08 +0100

    / ‘datetime_test.py’

--- datetime_test.py
+++ datetime_test.py
@@ -6,5 +6,5 @@ now = datetime.now()
 print("now =", now)
 
 # dd/mm/YY H:M:S
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("date and time =", dt_string)	
+dt_string = now.strftime("%d-%m-%Y_%H_%M_%S")
+print("date and time =", dt_string)
