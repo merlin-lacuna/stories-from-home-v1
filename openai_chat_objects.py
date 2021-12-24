06e3d6017d8c84a3d88570458a621ca43c86aabd
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:45:49 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -20,23 +20,24 @@ conversation = ["The following is a conversation between two dwarves who are ang
                 start_sequence
                 ]
 
-print("".join(conversation))
+theprompt = "".join(conversation)
 
-# theprompt = "The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants." + start_sequence
-#
-# completion=oa.Completion.create(
-#   engine="davinci",
-#   prompt=theprompt,
-#   temperature=0.9,
-#   max_tokens=150,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0.6,
-#   stop=["\n", "Bob:", "Barry:"]
-# )
-#
-# bobsanswer = completion.choices[0].text
-# print("Bob:", bobsanswer)
+completion=oa.Completion.create(
+  engine="davinci",
+  prompt=theprompt,
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=["\n", "Bob:", "Barry:"]
+)
+
+bobsanswer = completion.choices[0].text
+
+conversation.append(bobsanswer + restart_sequence)
+
+print("Bob:", bobsanswer)
 
 
 
