235d1701da6cf40d655e3f814d6e252cec83a894
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:33:30 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -25,27 +25,27 @@ completion=oa.Completion.create(
   stop=["\n", "Bob:", "Barry:"]
 )
 
-utterance = completion.choices[0].text
-print("Bob:", utterance)
+bobsanswer = completion.choices[0].text
+print("Bob:", bobsanswer)
 
 ##
 
-# start_sequence = "\nBarry:"
-# restart_sequence = "\nBob:"
-#
-# completion=oa.Completion.create(
-#   engine="davinci",
-#   prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry:"+barrysanswer,
-#   temperature=0.9,
-#   max_tokens=150,
-#   top_p=1,
-#   frequency_penalty=0,
-#   presence_penalty=0.6,
-#   stop=["\n", "Bob:", "Barry:"]
-# )
-#
-# utterance = completion.choices[0].text
-# print("Barry:", utterance)
+start_sequence = "\nBarry:"
+restart_sequence = "\nBob:"
+
+completion=oa.Completion.create(
+  engine="davinci",
+  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants." + restart_sequence + bobsanswer + start_sequence,
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=["\n", "Bob:", "Barry:"]
+)
+
+barrysanswer = completion.choices[0].text
+print("Barry:", barrysanswer)
 
 # while x < 11:
 #   x = x+1
