798ef84780643ccfdeb839db2a0f2ed353292d35
Chaz McGinty <bookings@gmail.com>
2021-12-24 21:49:08 +0100

    / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -5,12 +5,17 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Engine.list()
 
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 start_sequence = "\nBob:"
-restart_sequence = "\nBarry:"
+restart_sequence = "\nBarry: "
 
 response = openai.Completion.create(
   engine="davinci",
-  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob: That's a great idea Barry, maybe we can get an export reduction!\nBarry: ",
+  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.",
   temperature=0.9,
   max_tokens=150,
   top_p=1,
@@ -18,6 +23,8 @@ response = openai.Completion.create(
   presence_penalty=0.6,
   stop=["\n", "Bob:", "Barry:"]
 )
+
+## 
 
 start_sequence = "\nBarry:"
 restart_sequence = "\nBob: "
