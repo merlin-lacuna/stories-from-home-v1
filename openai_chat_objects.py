b60cdd05ff5d6f63d6fcf02ef36aa879b619997e
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:05:29 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -9,7 +9,7 @@ restart_sequence = "\nBarry: "
 
 response = openai.Completion.create(
   engine="davinci",
-  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.",
+  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob:",
   temperature=0.9,
   max_tokens=150,
   top_p=1,
@@ -17,7 +17,5 @@ response = openai.Completion.create(
   presence_penalty=0.6,
   stop=["\n", "Bob:", "Barry:"]
 )
-
-{"prompt":"The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob:","max_tokens":150,"temperature":0.9,"top_p":1,"frequency_penalty":0,"presence_penalty":0.6,"best_of":1,"echo":true,"logprobs":0,"stop":["\n","Bob:","Barry:"],"stream":true}
 
 print("R:", response.choices)
