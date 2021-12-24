4eeed8303eaa27f625229c262962ef89f243b3f3
Chaz McGinty <bookings@gmail.com>
2021-12-24 21:46:50 +0100

    / ‘openai_chat_objects.py’ / ‘openai_test.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -5,4 +5,16 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Engine.list()
 
-openai_test.py
+start_sequence = "\nBob:"
+restart_sequence = "\nBarry: "
+
+response = openai.Completion.create(
+  engine="davinci",
+  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob: That's a great idea Barry, maybe we can get an export reduction!\nBarry: ",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=["\n", "Bob:", "Barry:"]
+)
--- openai_test.py
+++ openai_test.py
@@ -3,4 +3,4 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Engine.list()
+print(openai.Engine.list())
