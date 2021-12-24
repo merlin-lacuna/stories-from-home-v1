ac5c9d6b3f52dcbae976ea38777a1f56044f3e11
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:41:43 +0100

    / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -12,22 +12,6 @@ x = 0
 bobsanswer = ""
 barrysanswer = ""
 
-theprompt = "The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants." + start_sequence
-
-completion=oa.Completion.create(
-  engine="davinci",
-  prompt=theprompt,
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=["\n", "Bob:", "Barry:"]
-)
-
-bobsanswer = completion.choices[0].text
-print("Bob:", bobsanswer)
-
 conversation = ["The following is a conversation between two dwarves who are angry about the price of gold\n.",
                 "\nBob: The price of gold is too damn high! Don't you agree Barry?",
                 "\nBarry: Totally mate, I can't afford any more gold for my treasury.",
@@ -36,6 +20,24 @@ conversation = ["The following is a conversation between two dwarves who are ang
                 ]
 
 print("".join(conversation))
+
+# theprompt = "The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants." + start_sequence
+#
+# completion=oa.Completion.create(
+#   engine="davinci",
+#   prompt=theprompt,
+#   temperature=0.9,
+#   max_tokens=150,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0.6,
+#   stop=["\n", "Bob:", "Barry:"]
+# )
+#
+# bobsanswer = completion.choices[0].text
+# print("Bob:", bobsanswer)
+
+
 
 ##
 
