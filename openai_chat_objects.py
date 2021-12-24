ca5040fe5f1b0589fdadef5428bd278ff682a482
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:20:02 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -5,38 +5,47 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 oa = openai
+
 start_sequence = "\nBob:"
 restart_sequence = "\nBarry: "
+x = 0
+bobsanswer = ""
+barrysanswer = ""
 
-completion=oa.Completion.create(
-  engine="davinci",
-  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob:",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=["\n", "Bob:", "Barry:"]
-)
 
-utterance = completion.choices[0].text
-print("UTTERANCE1:", utterance)
+while x < 11:
 
-##
+  completion=oa.Completion.create(
+    engine="davinci",
+    prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it? \nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.\nBob:"+bobsanswer,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=["\n", "Bob:", "Barry:"]
+  )
 
-start_sequence = "\nBarry:"
-restart_sequence = "\nBob: "
+  utterance = completion.choices[0].text
+  print("Bob:", utterance)
 
-completion=oa.Completion.create(
-  engine="davinci",
-  prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry:",
-  temperature=0.9,
-  max_tokens=150,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0.6,
-  stop=["\n", "Bob:", "Barry:"]
-)
+  ##
 
-utterance = completion.choices[0].text
-print("UTTERANCE2:", utterance)
+  start_sequence = "\nBarry:"
+  restart_sequence = "\nBob: "
+
+  completion=oa.Completion.create(
+    engine="davinci",
+    prompt="The following is a conversation between two dwarves who are angry about the price of gold.\n\nBob: The price of gold is too damn high! Don't you agree Barry?\nBarry: Totally mate, I can't afford any more gold for my treasury.\nBob: What do you think we should do about it?\nBarry:"+barrysanswer,
+    temperature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0.6,
+    stop=["\n", "Bob:", "Barry:"]
+  )
+
+  utterance = completion.choices[0].text
+  print("Barry:", utterance)
+
+  x = x+1
