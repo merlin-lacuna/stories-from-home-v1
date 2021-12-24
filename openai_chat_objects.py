0695602cc5439ab5735f19dec52489ddfd17327c
Chaz McGinty <bookings@gmail.com>
2021-12-24 22:42:16 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -17,6 +17,7 @@ conversation = ["The following is a conversation between two dwarves who are ang
                 "\nBarry: Totally mate, I can't afford any more gold for my treasury.",
                 "\nBob: What do you think we should do about it?"
                 "\nBarry: Let's form a miners guild, we'll get the best miners in our lands and put pressure on the gold merchants.",
+                start_sequence
                 ]
 
 print("".join(conversation))
