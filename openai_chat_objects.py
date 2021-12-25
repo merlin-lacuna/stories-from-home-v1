d4e7ccfeb81239dfc2999cccffd97ec328798130
Chaz McGinty <bookings@gmail.com>
2021-12-25 01:52:43 +0100

    / ‘.idea/workspace.xml’ / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -18,7 +18,7 @@ scenario = "The following is a conversation the EntityA and the EntityB who conv
 
 conversation = [
                 "\nEntityA: Flowing lymph and resting soil. I cover to protect and unveil to embrace. Layers of sedimented history stratify the present. My soul is a rocky concretion but my skin is a delicate crust that is being ravaged by a billion tiny ants.",
-                "\nEntityB: What is it that ravages you my dear? I have failed to protect you. Every day I become more opaque... a whisper, a flutter, a gust from the unknown. I have become a weapon that is turned against you, suffocating you like a lover whose body has become hot with fever. Do you feel the fever too?  Do you feel my sweat raining down upon your brow?",
+                "\nEntityB: What is it that ravages you my dear? I have failed to protect you. Every day I become more opaque... a whisper, a flutter, a gust from the unknown. I have become a weapon that is turned against you, suffocating you like a lover whose body has become hot with fever. Do you feel the fever too? Do you feel my sweat raining down upon your brow?",
                 ]
 
 
