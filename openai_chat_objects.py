45f3434ef7b3d135fc55c51ee05edd31bc8721fa
Chaz McGinty <bookings@gmail.com>
2021-12-25 02:18:17 +0100

    / ‘openai_chat_objects.py’

--- openai_chat_objects.py
+++ openai_chat_objects.py
@@ -17,17 +17,10 @@ nextspeaker = "entity-a"
 scenario = "The following is a conversation the EntityA and the EntityB who converse in a delirious, poetic manner. The EntityA feels sick and the EntityB is trying to find out what is wrong.\n"
 
 conversation = [
-                "\nEntityA: because of this set of ecological catastrophes we no longer have the luxury of believing in a clear  division between these two domains. Do you think we are seeing, as a result of this  collapse, a broader epistemic shift in the sciences towards alternate world-making  devices, such as narration and storytelling?",
+                "\nEntityA: Flowing lymph and resting soil. I cover to protect and unveil to embrace. Layers of sedimented history stratify the present. My soul is a rocky concretion but my skin is a delicate crust that is being ravaged by a billion tiny ants.",
                 "\nEntityB: What is it that ravages you my dear? I have failed to protect you. Every day I become more opaque... a whisper, a flutter, a gust from the unknown. I have become a weapon that is turned against you, suffocating you like a lover whose body has become hot with fever. Do you feel the fever too? Do you feel my sweat raining down upon your brow?",
                 ]
-conversationhtml = []
-# Academic blathering:
-# scenario = "The following is a conversation the EntityA and the EntityB who converse in a delirious, poetic manner.\n"
-#
-# conversation = [
-#                 "\nEntityA: Because of a set of ecological catastrophes we no longer have the luxury of believing in a clear division between the humanities and science. Do you think we are seeing, as a result of this  collapse, a broader epistemic shift in the sciences towards alternate world-making  devices, such as narration and storytelling?",
-#                 "\nEntityB: I think it’s wishful thinking on the part of the people from the humanities that the sciences have changed that much. But it is a useful kind of  wishful thinking because it is a way to move the argument out of the standard  situation where you have people like us in science studies saying that science and politics have always been intermingled.",
-#                 ]
+
 
 while x < 11:
     if(nextspeaker == "entity-a"):
@@ -64,10 +57,3 @@ while x < 11:
 print("------------------------------")
 for c in conversation:
     print(c.strip())
-    conversationhtml.append("<p>"+c.strip()+"</p>")
-
-data = '\n'.join(conversation)
-finalfile = './generations/' + dt_string + '.txt'
-with open(finalfile, 'w') as f:
-    with redirect_stdout(f):
-        print(data)
