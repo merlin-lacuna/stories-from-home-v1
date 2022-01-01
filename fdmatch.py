f819ddad1cf5226cca4385fc30a39de90d8004f8
Chaz McGinty <bookings@gmail.com>
2022-01-01 20:35:49 +0100

    / ‘.idea/workspace.xml’ / ‘fdmatch.py’

--- fdmatch.py
+++ fdmatch.py
@@ -5,6 +5,9 @@ from scipy.spatial.distance import euclidean
 x = np.array([1, 2, 3, 3, 7])
 y = np.array([1, 2, 2, 2, 2, 2, 2, 4])
 
+a = np.array([1, 2, 3, 3, 3, 3, 3,5])
+b = np.array([1, 2, 2, 2, 2, 2, 2, 4])
+
 distance, path = fastdtw(x, y, dist=euclidean)
 
 print(distance)
