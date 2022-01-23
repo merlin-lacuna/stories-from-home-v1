ea712950b277720e41b14fb4bdc81b54ecbc7606
Chaz McGinty <bookings@gmail.com>
2022-01-23 22:22:33 +0100

    / ‘.idea/workspace.xml’ / ‘openai_test_single_generation.py’

--- openai_test_single_generation.py
+++ openai_test_single_generation.py
@@ -1,19 +1,31 @@
 import openai
 import os
 from dotenv import load_dotenv
+from contextlib import redirect_stdout
+from datetime import datetime
 load_dotenv()
+
+# datetime object containing current date and time
+now = datetime.now()
+dt_string = now.strftime("%d-%m-%Y_%H_%M_%S")
 
 oa = openai
 oa.api_key = os.getenv("OPENAI_API_KEY")
 completion = oa.Completion.create(
-    engine="davinci-instruct-beta-v3",
-    prompt=storypromptbase + storypromptextra,
-    temperature=0.7,
-    max_tokens=90,
+    engine="text-davinci-001",
+    prompt="Write an essay in the style of David Foster Wallace written from the perspective of a volcano.",
+    temperature=1,
+    max_tokens=600,
     top_p=1,
     frequency_penalty=0.5,
-    presence_penalty=0
+    presence_penalty=0.5
 )
 
 # print the completion
 story = completion.choices[0].text
+print(story)
+
+finalfile = './generations/' + dt_string + '.txt'
+with open(finalfile, 'w') as f:
+    with redirect_stdout(f):
+        print(story)
