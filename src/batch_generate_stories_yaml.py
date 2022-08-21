7de4d5e2f88a5c56f5f04fcb6f75560db445d4e4
Merlin Carter <mcarter@gmail.com>
2022-08-21 17:57:33 +0200

    adding new charts

--- src/batch_generate_stories_yaml.py
+++ src/batch_generate_stories_yaml.py
@@ -9,7 +9,7 @@ from ruamel.yaml import YAML
 #### LOAD ENTITY CONFIG
 yaml=YAML(typ='safe')
 yaml.default_flow_style = False
-configfile="../data/water_land_ndwi_hongkong.yaml"
+configfile="../data/earth_land_ndsi_poland.yaml"
 
 with open(configfile, encoding='utf-8') as f:
    econfig = yaml.load(f)
